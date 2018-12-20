e36a3b10cdb293712f29013fb7be2a62b05cf109
Morgan Costigan <morgan.costigan@gmail.com>
2018-12-20 11:23:08 -0600

    comment notes added

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -7,7 +7,7 @@ const userStrategy = require('../strategies/user.strategy');
 const router = express.Router();
 
 // Handles Ajax request for user information if user is authenticated
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => { //rejects user if they don't have the proper cookie
   // Send back user object from the session (previously queried from the database)
   res.send(req.user);
 });
--- src/redux/sagas/loginSaga.js
+++ src/redux/sagas/loginSaga.js
@@ -9,7 +9,7 @@ function* loginUser(action) {
 
     const config = {
       headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
+      withCredentials: true,  //this needs to be true to use cookies
     };
 
     // send the action.payload as the body
